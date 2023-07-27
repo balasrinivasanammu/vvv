c5414cb91acf1f9306e493f42cd54df51135874c
balasrinivasanammu <balajiit88@gmail.com>
2023-07-27 19:21:43 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+main()
+{
+	int a[]={10,20,30};
+	int i;
+	for(i=-3;i<3;i++)
+	{
+		printf("%d \n",a[i]);
+	}
+	/*a[0]=a[1]*a[2];
+	printf("%d\n",a[0]);
+	for(i=0;i<3;i++)
+	{
+		printf("%d \n",a[i]);
+	}*/
+}
